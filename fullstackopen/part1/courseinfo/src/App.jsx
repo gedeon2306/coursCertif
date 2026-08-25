c9ce3266eb1a085e1f4abea857983f0d970f3eb6
GANGOUE MBIMI jihrel Gédéon <gedeon.jireh.gangoue23@gmail.com>
2026-08-25 15:52:03 +0100

    fullstackopen partie 1 exercices 2

--- fullstackopen/part1/courseinfo/src/App.jsx
+++ fullstackopen/part1/courseinfo/src/App.jsx
@@ -2,18 +2,20 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
@@ -26,7 +28,6 @@ const Total = (props) => {
   )
 }
 
-// Composant principal
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
