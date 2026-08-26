eb1c3cbb612dc9b6eec29065114954fef7e5f041
GANGOUE MBIMI jihrel Gédéon <gedeon.jireh.gangoue23@gmail.com>
2026-08-26 20:07:40 +0100

    fullstackopen partie 1 exercices 8

--- fullstackopen/part1/unicafe/src/App.jsx
+++ fullstackopen/part1/unicafe/src/App.jsx
@@ -1,5 +1,25 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / total
+  const positivePercentage = total === 0 ? 0 : (good / total) * 100
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercentage} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -9,10 +29,6 @@ const App = () => {
   const handleNeutralClick = () => setNeutral(neutral + 1)
   const handleBadClick = () => setBad(bad + 1)
 
-  const total = good + neutral + bad
-  const average = total === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / total
-  const positivePercentage = total === 0 ? 0 : (good / total) * 100
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -20,13 +36,7 @@ const App = () => {
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
 
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positivePercentage} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
