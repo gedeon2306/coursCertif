185df6c9c27ae4a898b626537329ceccc5520651
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-08-28 21:30:43 +0100

    fullstackopen partie 2 exercices 11

--- fullstackopen/part2/phonebook/src/App.jsx
+++ fullstackopen/part2/phonebook/src/App.jsx
@@ -1,61 +1,57 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-// Composant pour le champ de recherche
-const Filter = ({ filter, handleFilterChange }) => {
-  return (
-    <div>
-      filter shown with <input value={filter} onChange={handleFilterChange} />
-    </div>
-  )
-}
+const Filter = ({ filter, handleFilterChange }) => (
+  <div>
+    filter shown with <input value={filter} onChange={handleFilterChange} />
+  </div>
+)
 
-// Composant pour le formulaire d'ajout
 const PersonForm = ({
   addPerson,
   newName,
   handleNameChange,
   newNumber,
   handleNumberChange
-}) => {
-  return (
-    <form onSubmit={addPerson}>
-      <div>
-        name: <input value={newName} onChange={handleNameChange} />
-      </div>
-      <div>
-        number: <input value={newNumber} onChange={handleNumberChange} />
-      </div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-  )
-}
-
-// Composant pour afficher la liste des personnes
-const Persons = ({ personsToShow }) => {
-  return (
+}) => (
+  <form onSubmit={addPerson}>
     <div>
-      {personsToShow.map((person) => (
-        <p key={person.id}>
-          {person.name} {person.number}
-        </p>
-      ))}
+      name: <input value={newName} onChange={handleNameChange} />
     </div>
-  )
-}
+    <div>
+      number: <input value={newNumber} onChange={handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
+const Persons = ({ personsToShow }) => (
+  <div>
+    {personsToShow.map((person) => (
+      <p key={person.id}>
+        {person.name} {person.number}
+      </p>
+    ))}
+  </div>
+)
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  // Initialisation à un tableau vide
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  // Hook d'effet pour charger les données depuis le serveur json-server
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        setPersons(response.data)
+      })
+  }, []) // Le tableau vide [] garantit que l'effet ne s'exécute qu'une seule fois au montage
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
