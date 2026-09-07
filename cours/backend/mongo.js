67ca66a9297f9ffec7de8c605dd33ba60bc44c1f
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-09-07 19:46:52 +0100

    mongodb

---
+++ cours/backend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+ console.log('Please provide the password as an argument: node mongo.js <password>')
+ process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstackopen:${password}@cluster0.fvrn9j6.mongodb.net/?appName=noteApp`
+
+const noteSchema = new mongoose.Schema({
+ content: String,
+ date: Date,
+ important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+mongoose
+ .connect(url)
+ .then((result) => {
+   console.log('connected')
+
+  //  const note = new Note({
+  //    content: 'HTML is Easy 2',
+  //    date: new Date(),
+  //    important: true,
+  //  })
+
+  //  return note.save()
+
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+ })
+ .then(() => {
+   console.log('note saved!')
+   return mongoose.connection.close()
+ })
+ .catch((err) => console.log(err))
