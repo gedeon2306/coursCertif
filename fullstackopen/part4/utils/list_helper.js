583396b1d37dd06e2712ad7369b7e1e8b940ef06
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-09-14 19:38:01 +0100

    part 4 exercice 4.6

--- fullstackopen/part4/utils/list_helper.js
+++ fullstackopen/part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -23,8 +25,27 @@ const favoriteBlog = (blogs) => {
   }
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const blogsByAuthor = _.groupBy(blogs, 'author')
+
+  const authorWithMostBlogs = _.maxBy(
+    Object.keys(blogsByAuthor).map(author => ({
+      author: author,
+      blogs: blogsByAuthor[author].length
+    })),
+    'blogs'
+  )
+
+  return authorWithMostBlogs
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs
 }
