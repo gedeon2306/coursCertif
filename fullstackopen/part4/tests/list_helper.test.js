953c0da97fa7d2e0e62c1e00f106051065d964c3
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-09-14 19:27:13 +0100

    part 4 exercice 4.5

--- fullstackopen/part4/tests/list_helper.test.js
+++ fullstackopen/part4/tests/list_helper.test.js
@@ -63,3 +63,55 @@ describe('total likes', () => {
     expect(result).toBe(24)
   })
 })
+
+describe('favorite blog', () => {
+  const blogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/users/ewd/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toBeNull()
+  })
+
+  test('when list has only one blog, returns that blog info', () => {
+    const result = listHelper.favoriteBlog([blogs[0]])
+    expect(result).toEqual({
+      title: 'React patterns',
+      author: 'Michael Chan',
+      likes: 7
+    })
+  })
+
+  test('of a bigger list is identified correctly', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    })
+  })
+})
--- fullstackopen/part4/utils/list_helper.js
+++ fullstackopen/part4/utils/list_helper.js
@@ -7,7 +7,24 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const favorite = blogs.reduce((prev, current) => {
+    return (prev.likes > current.likes) ? prev : current
+  })
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
