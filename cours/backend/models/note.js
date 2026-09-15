22c8b6abcb11097eca98de8f853e6242e4f7f6ae
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-09-15 16:04:10 +0100

    tests d'integration 1

--- cours/backend/models/note.js
+++ cours/backend/models/note.js
@@ -8,6 +8,7 @@ const noteSchema = new mongoose.Schema({
   },
   date: { 
     type: Date,
+    default: Date.now,
     required: true
   },
   important: Boolean
--- cours/backend/utils/middleware.js
+++ cours/backend/utils/middleware.js
@@ -1,6 +1,6 @@
 const logger = require('./logger')
 
-const requestLogger = (request, next) => {
+const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
   logger.info('Body:  ', request.body)
@@ -8,11 +8,11 @@ const requestLogger = (request, next) => {
   next()
 }
 
-const unknownEndpoint = (response) => {
+const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-const errorHandler = (error, response, next) => {
+const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
   if (error.name === 'CastError') {
