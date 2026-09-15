f6744a227b064a60ea4b63d448e94903cd323388
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-09-15 21:37:34 +0100

    partie 4 exercice 4.10

--- fullstackopen/part4/tests/blog_api.test.js
+++ fullstackopen/part4/tests/blog_api.test.js
@@ -8,7 +8,7 @@ const helper = require('./test_helper')
 beforeEach(async () => {
   await Blog.deleteMany({})
   await Blog.insertMany(helper.initialBlogs)
-})
+}, 10000)
 
 test('blogs are returned as json and correct amount is returned', async () => {
   const response = await api
@@ -25,6 +25,27 @@ test('unique identifier property of the blog posts is named id', async () => {
   // Vérifie le premier élément du tableau
   expect(response.body[0].id).toBeDefined()
   expect(response.body[0]._id).toBeUndefined()
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Async/await simplifies async code',
+    author: 'jihreldev',
+    url: 'https://jihreldev.vercel.app/',
+    likes: 10,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(b => b.title)
+  expect(titles).toContain('Async/await simplifies async code')
 })
 
 afterAll(async () => {
